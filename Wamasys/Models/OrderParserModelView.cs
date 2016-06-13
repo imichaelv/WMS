e2d317b63075f1dd97cee5ee1770ac4bef99e85b
metaladdict6 <r.r.c.ziengs@st.hanze.nl>
2016-06-13 14:35:24 +0200

    Start of the API

---
+++ Wamasys/Models/OrderParserModelView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using XSerializer;
+
+namespace Wamasys.Models
+{
+    public class OrderParserModelView
+    {   
+        public void recieveOrder(XmlDocument xmlfile)
+        {
+            XSerializerXmlReader Parser = new XSerializerXmlReader();
+        }
+
+        public void InsertOrder()
+        {
+
+        }
+    }
+}
