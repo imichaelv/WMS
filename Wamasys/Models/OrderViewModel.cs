a4b76c7ff62b8e2b950d69606e0641abbc70a034
Leon Wetzel <leonwetzel@gmail.com>
2016-06-13 10:02:27 +0200

    Added a few models and altered .cshtml files

---
+++ Wamasys/Models/OrderViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Wamasys.Models
+{
+    /// <summary>
+    /// Contains attributes for all Order types.
+    /// </summary>
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+    }
+
+    /// <summary>
+    /// Contains atributes for orders from suppliers.
+    /// </summary>
+    public class SupplierOrderViewModel : OrderViewModel
+    {
+        public int Amount { get; set; }
+
+        public int StatusId { get; set; }
+    }
+}
---
+++ Wamasys/Models/StatusViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wamasys.Models
+{
+    public class StatusViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
