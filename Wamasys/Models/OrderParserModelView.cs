4ace8287af9890e86dd97160f65897ba789ee1ac
metaladdict6 <r.r.c.ziengs@st.hanze.nl>
2016-06-16 11:50:52 +0200

    Added the basic functionality for database interaction

--- Wamasys/Models/OrderParserModelView.cs
+++ Wamasys/Models/OrderParserModelView.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Web.Mvc;
 using System.Xml;
+using Wamasys.Models.Database;
 
 namespace Wamasys.Models
 {
     public class OrderParserModelView
     {
         StringBuilder output = new StringBuilder();
+        public static int NextOrderId;
+
+        public OrderParserModelView()
+        {
+            NextOrderId = getNexOrderId();
+        }
+
+        private int getNexOrderId()
+        {
+            return 1;
+        }
 
         public void recieveOrder(string input)
         {
@@ -39,10 +53,33 @@ namespace Wamasys.Models
             }
         }
 
-
-        public void InsertOrder(MakeOrderModel customerOrder)
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async void InsertOrder(MakeOrderModel model)
         {
+            List<Item> items = new List<Item>();
+            var customerOrder = new CustomerOrder();
 
+            customerOrder.CustomerOrderid = NextOrderId;
+            NextOrderId++;
+            customerOrder.Company.CompanyId = model.CustemorId;
+            customerOrder.Date = model.datetime;
+            customerOrder.Status.StatusId = model.StatusId;
+            int stopPoint = model.Amount;
+            foreach(Item item in items)
+            {
+                if(item.CustomerOrderId == 0)
+                {
+                    item.GantryId = 0;
+                    item.CustomerOrderId = customerOrder.CustomerOrderid;
+                    stopPoint--;
+                    if(stopPoint == 0)
+                    {
+                        break;
+                    }
+                }
+            }
         }
     }
 
