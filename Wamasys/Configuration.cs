20f364edfab8c4a08499afc9b92fd6d3284bb6c3
Daniel-Slobben <d.j.slobben@st.hanze.nl>
2016-06-22 13:02:33 +0200

    added correct  class

--- Wamasys/Configuration.cs
+++ Wamasys/Configuration.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity.Migrations;
+using Wamasys.Models.Database;
 
 namespace Wamasys.Migrations
 {
-    public class Configuration
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
-    }
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            // Leveranciers
+            var leveranciers = new List<Supplier>
+            {
+                new Supplier {Name = "Fabriek Groningen", HouseNumber = "515", City = "Groningen"};
+            }
+        }
+    }    
 }
