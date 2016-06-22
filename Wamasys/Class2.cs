cc89009104f259c23815e5d53aa4eab964b7e656
Daniel-Slobben <d.j.slobben@st.hanze.nl>
2016-06-22 15:19:14 +0200

    Added accounts

---
+++ Wamasys/Class2.cs
@@ -0,0 +1,9 @@
+﻿namespace Wamasys.Models.Values
+{
+    public class RoleModel
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+        public const string Supplier = "Supplier";
+    }
+}
