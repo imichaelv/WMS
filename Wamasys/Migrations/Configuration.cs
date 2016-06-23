c85265bb6a35d56ce3ec649a8ad3c47db5cc8137
Sergen <sergen_nurel@hotmail.com>
2016-06-23 02:30:09 +0200

    removed

--- Wamasys/Migrations/Configuration.cs
+++ Wamasys/Migrations/Configuration.cs
@@ -5,14 +5,12 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 using Wamasys.Models.Database;
 using Wamasys.Models.Values;
+using System;
+using System.Data.Entity.Migrations;
+using System.Linq;
 
 namespace Wamasys.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
     internal sealed class Configuration : DbMigrationsConfiguration<Wamasys.ApplicationDbContext>
     {
 
